957dbfeba117097955cdf6e12f39265530d6ef0b
xxwy007 <2846395623e@qq.com>
2023-08-15 10:51:31 +0800

    修改文件名

--- windows/socekt/client.cpp
+++
@@ -1,9 +0,0 @@
-#include "MySocket.h"
-int main()
-{
-	CTcpClient* client = new CTcpClient();
-	//client->ConnectToServer("1.14.16.83", "8090");
-	client->ConnectToServer("127.0.0.1", "8090");
-	Sleep(10000);
-	return 0;
-}
--- windows/socekt/server.cpp
+++
@@ -1,8 +0,0 @@
-#include "MySocket.h"
-int main()
-{
-	CTcpServer* server = new CTcpServer();
-	server->InitServer("8090");
-	server->Accept();
-	return 0;
-}
